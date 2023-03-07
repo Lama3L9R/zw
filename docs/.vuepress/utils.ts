c1ecc7f2d94d383ae2d8621f5e1b2f37d1bdac14
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-03-07 08:09:00 +0800

    Delete utils.ts

--- docs/.vuepress/utils.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-import { resolve } from 'path'
-
-export const resolvePages: (directory: string, filter?: (file) => boolean) => { prefix: string, children: string[] } = (directory, filter) => {
-    return { prefix: directory, children: fs.readdirSync(resolve(__dirname, '../', directory)).filter(filter ?? ((i) => i.endsWith('.md')) ) }
-}
