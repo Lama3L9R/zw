debb5b679280c3950d7af3df1a7f22689f15ee40
Lama3L9R <i@lama.icu>
2023-03-07 23:34:20 +0800

    修复 navbar 不生效bug

--- docs/.vuepress/categories.ts
+++ docs/.vuepress/categories.ts
@@ -1,9 +1,9 @@
 
 const categories: { directory: string, displayName: string, filter?: (file: string) => boolean  }[] = [
-    { directory: '1A', displayName: "电动篇" },
-    { directory: '1B', displayName: "生活篇" },
-    { directory: '1C', displayName: "人生哲理篇" },
-    { directory: '1D', displayName: "祝福篇" }
+    { directory: '1A/', displayName: "电动篇" },
+    { directory: '1B/', displayName: "生活篇" },
+    { directory: '1C/', displayName: "人生哲理篇" },
+    { directory: '1D/', displayName: "祝福篇" }
 ]
 
 export default categories
