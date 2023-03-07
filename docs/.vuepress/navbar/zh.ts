76a5088f904dd340d49a3019cc5477204c60a89b
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-03-07 08:07:06 +0800

    Update zh.ts

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -1,6 +1,5 @@
 import { navbar } from "vuepress-theme-hope";
-import categories from "../categories";
-import { resolvePages } from "../utils";
+import { version } from "../version";
 
 export const zhNavbar = navbar([
   "/",
@@ -14,11 +13,26 @@ export const zhNavbar = navbar([
         prefix: "/",
         link: "content.md",
       },
-      ...(function*() {
-        for (const i of categories) {
-          yield { text: i.displayName, icon: "more", ...resolvePages(i.directory, i.filter) }  
-        }
-      })()
+      {
+        text: "电动篇",
+        prefix: "1A/",
+        children: ["01.md", "02.md", "03.md", "04.md"],
+      },
+      {
+        text: "生活篇",
+        prefix: "1B/",
+        children: ["01.md", "02.md", "03.md", "04.md", "05.md", "06.md", "07.md", "08.md", "09.md", "10.md", "11.md", "12.md", "13.md"],
+      },
+      {
+        text: "人生哲理篇",
+        prefix: "1C/",
+        children: ["01.md"],
+      },
+      {
+        text: "祝福篇",
+        prefix: "1D/",
+        children: ["01.md"],
+      },
     ],
   },
 ]);
