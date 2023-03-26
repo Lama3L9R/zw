21598814a0c4c156770fb6ea912d302aaf4bb3ec
Lama3L9R <i@lama.icu>
2023-03-26 16:55:05 +0800

    添加更多文章和一个全新的章节

--- docs/.vuepress/categories.ts
+++ docs/.vuepress/categories.ts
@@ -3,7 +3,8 @@ const categories: { directory: string, displayName: string, filter?: (file: stri
     { directory: '1A/', displayName: "电动篇" },
     { directory: '1B/', displayName: "生活篇" },
     { directory: '1C/', displayName: "人生哲理篇" },
-    { directory: '1D/', displayName: "祝福篇" }
+    { directory: '1D/', displayName: "祝福篇" },
+    { directory: '1E/', displayName: "外星人篇" }
 ]
 
 export default categories
